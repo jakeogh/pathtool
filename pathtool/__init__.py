c687a94265945b852721bdd07515b48eb96c0eb0
jakeogh <github.com@v6y.net>
2021-06-15 13:15:52 -0700

    auto-commit

--- pathtool/__init__.py
+++ pathtool/__init__.py
@@ -1,4 +1,5 @@
 from .pathtool import empty_file
+from .pathtool import get_file_size
 from .pathtool import get_path_with_most_free_space
 from .pathtool import path_exists
 from .pathtool import path_is_file
