ad2e181f5a3267da157265b22c9ea50d155066bd
jakeogh <github.com@v6y.net>
2021-07-02 19:17:49 -0700

    auto-commit

--- pathtool/pathtool.py
+++ pathtool/pathtool.py
@@ -23,6 +23,7 @@ import fcntl
 import os
 import shutil
 import stat
+import subprocess
 import sys
 import tempfile
 import time
@@ -49,10 +50,14 @@ from typing import Optional
 from typing import Sequence
 
 from enumerate_input import enumerate_input
+from psutil import disk_usage
+
+signal(SIGPIPE,SIG_DFL)
 
 #from with_chdir import chdir
 
-
+#from pathtool import path_is_block_special
+#from getdents import files
 
 
 def eprint(*args, **kwargs):
@@ -93,25 +98,279 @@ def validate_slice(slice_syntax: str):
 #    return False
 
 
-from signal import SIG_DFL
-from signal import SIGPIPE
-from signal import signal
+def path_is_dir(path):
+    if os.path.isdir(path):  # could still be a symlink
+        if os.path.islink(path):
+            return False
+        return True
+    return False
 
-from psutil import disk_usage
 
-signal(SIGPIPE,SIG_DFL)
-from typing import ByteString
-from typing import Generator
-from typing import Iterable
-from typing import List
-from typing import Optional
-from typing import Sequence
+def target_generator(target_list, min_free_space, verbose=True):
+    if verbose:
+        ic(min_free_space)
+    for target in target_list:
+        if verbose:
+            ic(target)
+        if path_exists(target):
+            assert path_is_dir(target)
+            free_space = disk_usage(target).free
+            ic(free_space)
+            if disk_usage(target).free >= min_free_space:
+                yield target
+            else:
+                eprint("skipped:", target, "<", min_free_space)
+    raise FileNotFoundError
 
-#from with_chdir import chdir
 
-#from pathtool import path_is_block_special
-#from getdents import files
+#
+#
+#def count_entries(folder):  # fast, returns all types of objects in a folder
+#    return len(os.listdir(folder))
+#
+#
 
+
+#def count_files(folder):  # calls lstat on every entry to see if its a file
+#    total = 0
+#    for root, dirs, files in os.walk(folder):
+#        total += len(files)
+#    return total
+#
+#
+#def list_files(folder):
+#    all_files = []
+#    for root, dirs, files in os.walk(folder):
+#        for ifile in files:
+#            relative_file_path = root + b'/' + ifile
+#            all_files.append(relative_file_path)
+#    return set(all_files)
+
+
+#def path_is_dir(path):
+#    if os.path.isdir(path): #could still be a symlink
+#        if os.path.islink(path):
+#            return False
+#        return True
+#
+#
+#def path_is_dir_or_symlink_to_dir(path):
+#    # unlike os.path.exists(False), os.path.isdir(False) returns False
+#    if os.path.isdir(path): # returns False if it's a symlink to a file
+#        return True
+#    return False
+
+
+
+def is_symlink(infile):
+    if os.path.islink(infile):
+        return True
+    return False
+
+
+def is_symlink_to_dir(link):
+    if os.path.islink(link):
+        return os.path.isdir(link)
+    return False
+
+
+#def is_broken_symlink(path):
+#    if os.path.islink(path): # path is a symlink
+#        return not os.path.exists(path) # returns False for broken symlinks
+#    return False # path isnt a symlink
+#
+#
+#def is_unbroken_symlink(path):
+#    if os.path.islink(path): # path is a symlink
+#        return os.path.exists(path) # returns False for broken symlinks
+#    return False # path isnt a symlink
+
+
+def get_symlink_abs_target(link): # assumes link is unbroken
+    target = os.readlink(link)
+    target_joined = os.path.join(os.path.dirname(link), target)
+    target_file = os.path.realpath(target_joined)
+    return target_file
+
+
+# TODO get is_unbroken_symlink "from symlinktree import is_unbroken_symlink"
+#def is_unbroken_symlink_to_target(target, link):    #bug, should not assume unicode paths
+#    if is_unbroken_symlink(link):
+#        link_target = get_symlink_abs_target(link)
+#        if link_target == target:
+#            return True
+#    return False
+
+
+def calculate_relative_symlink_dest(target, link_name):
+    # todo eval https://docs.python.org/3/library/os.path.html#os.path.commonpath
+    if isinstance(target, str):
+        target = bytes(target, encoding='UTF8')
+    if isinstance(target, Path):
+        target = os.fsencode(target)
+
+    if isinstance(link_name, str):
+        link_name = bytes(link_name, encoding='UTF8')
+    if isinstance(link_name, Path):
+        link_name = os.fsencode(link_name)
+
+    # paths are bytes. this must work for all possible paths
+    assert isinstance(target, bytes)
+    assert isinstance(link_name, bytes)
+    #ceprint("target:", target)
+
+    assert not target.startswith(b'../')
+    # got relative target, that's hard to deal with pass in a fully qualified path
+
+    # if target is also an existing symlink, detect that and dont call realpath()
+    # call something that gets the realpath but does not follow any links
+
+    if is_unbroken_symlink(target):
+        # the target is also a symlink, dont resolve it, just get it's abspath
+        target_realpath = os.path.abspath(target)
+        # still a problem, since this was not fully resolved, it may still have symlinks embedded in it
+        # get the folder, resolve that since it's guranteed not to be a symlink
+        target_realpath_folder = b'/'.join(target_realpath.split(b'/')[:-1])
+        #ceprint("target_realpath_folder:", target_realpath_folder)
+        target_realpath_file = target_realpath.split(b'/')[-1]
+        #ceprint("target_realpath_file:", target_realpath_file)
+        target_realpath_folder_realpath = os.path.realpath(target_realpath_folder)
+        target_realpath = os.path.join(target_realpath_folder_realpath, target_realpath_file)
+        # uug. ok now.
+
+    elif path_exists(target):
+        # target is prob a file or dir, but could still be a broken symlink
+        target_realpath = os.path.realpath(target)
+
+    elif is_broken_symlink(link_name):
+        assert False
+
+    else: # idk
+        assert False
+
+    if is_broken_symlink(link_name):
+        link_name_realpath = os.path.realpath(link_name)
+        #ceprint("link_name_realpath:", link_name_realpath)
+    elif not path_exists(link_name):
+        link_name_realpath = os.path.realpath(link_name)
+        #ceprint("link_name_realpath:", link_name_realpath)
+
+    # if its a unbroken symlink, and this is being used to see if its the shortest dest
+    # os.path.realpath() cant be called, because it resolves the existing link to the target
+    elif is_unbroken_symlink(link_name):
+        link_name_realpath = os.path.abspath(link_name)
+        #ceprint("link_name_realpath: (abspath)", link_name_realpath)
+        # at this point, all is still not well.
+        # link_name_realpath was actually constructed by abspath()
+        # so if its really on a different filesystem, the link
+        # might not reflect that.
+        # the solution is to call realpath() on link_name_realpath_folder
+        # since its not a symlink, this will work as expected
+    else:
+        assert False
+
+    if not path_exists(target_realpath):
+        ic(target_realpath, 'does not exist. Refusing to make broken symlink.')
+        raise FileNotFoundError
+
+    if is_broken_symlink(link_name_realpath):
+        ic(link_name_realpath, 'exists as a broken symlink. Remove it before trying to make a new symlink. Exiting.')
+        sys.exit(1)
+
+    link_name_realpath_folder = b'/'.join(link_name_realpath.split(b'/')[:-1])
+    #ceprint("link_name_realpath_folder:", link_name_realpath_folder)
+    link_name_realpath_folder_realpath = os.path.realpath(link_name_realpath_folder)
+    #ceprint("link_name_realpath_folder_realpath:", link_name_realpath_folder_realpath)
+    if not os.path.isdir(link_name_realpath_folder_realpath):
+        ic(link_name_realpath_folder_realpath, 'does not exist.')
+        raise FileNotFoundError
+
+    relative_target = os.path.relpath(target_realpath, link_name_realpath_folder_realpath)  # relpath does not access the filesystem
+    #ceprint("relative_target:", relative_target)
+    return relative_target
+
+
+def create_relative_symlink(target, link_name):
+    relative_target = calculate_relative_symlink_dest(target, link_name)
+    link_name_realpath = os.path.realpath(link_name)
+    os.symlink(relative_target, link_name_realpath)
+
+
+def symlink_destination(link): # broken for multi level symlinks
+    return os.path.realpath(link)
+    #ceprint("this function is unreliable. fix it. it can loop forever.")
+    #ceprint(link)
+    #"""
+    #Return absolute path for the destination of a symlink. This prob should be split into "first dest" and "final dest"
+    #"""
+    ##assert (os.path.islink(link))
+    #p = link
+    #while os.path.islink(p):
+    #    p = os.path.normpath(os.readlink(link))  # huah?
+    #    if os.path.isabs(p):
+    #        return p
+    #    else:
+    #        p = os.path.join(os.path.dirname(link), p)
+    #dest = os.path.realpath(p)
+    #return dest
+
+
+def readlinkf(path): # ugly
+    p = subprocess.Popen(['readlink', '-f', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    readlink_output, errors = p.communicate()
+    readlink_output_clean = readlink_output.strip()
+    if errors:
+        ic(errors)
+    else:
+        return readlink_output_clean
+
+
+def get_abs_path_of_first_symlink_target(path):
+    ic(path)
+    link_target = os.readlink(path)
+    ic(link_target)
+    #assert link_target
+    link_dir = os.path.dirname(path)
+    link_first_target_abs = os.path.join(link_dir, link_target)
+    #ceprint(link_first_target_abs)
+    link_first_target_abs_normpath = os.path.normpath(link_first_target_abs)
+    #ceprint(link_first_target_abs_normpath)
+    link_first_target_abs_normpath_abspath = os.path.abspath(link_first_target_abs_normpath)
+    ic(link_first_target_abs_normpath_abspath)
+    return link_first_target_abs_normpath_abspath
+
+
+def get_symlink_target_next(path):
+    assert os.path.islink(path)
+    target = os.readlink(path)
+    return target
+
+
+def get_symlink_target_final(path): #broken for bytes
+    if os.path.islink(path):
+        target = os.readlink(path)
+        target_joined = os.path.join(os.path.dirname(path), target)
+        target_file = readlinkf(target_joined).decode('UTF-8')
+    else:
+        target_file = readlinkf(path).decode('UTF-8')
+    #print("target_file:", target_file)
+    return target_file
+
+
+#def symlink_or_exit(target, link_name, confirm=False, verbose=False):
+#    if verbose:
+#        ic(target)
+#        ic(link_name)
+#
+#    if confirm:
+#        input("press enter to os.symlink({}, {})".format(target, link_name))
+#
+#    try:
+#        os.symlink(target, link_name)
+#    except Exception as e:
+#        print("Got Exception: %s", e)
+#        print("Unable to symlink link_name: %s to target: %s Exiting." % (link_name, target))
+#        os._exit(1)
 
 def is_broken_symlink(path):
     if os.path.islink(path):  # path is a symlink
@@ -733,73 +992,3 @@ def cli(ctx,
 
         if verbose:  # or simulate:
             ic(index, path)
-
-
-
-
-
-
-
-def path_is_dir(path):
-    if os.path.isdir(path):  # could still be a symlink
-        if os.path.islink(path):
-            return False
-        return True
-    return False
-
-
-def target_generator(target_list, min_free_space, verbose=True):
-    if verbose:
-        ic(min_free_space)
-    for target in target_list:
-        if verbose:
-            ic(target)
-        if path_exists(target):
-            assert path_is_dir(target)
-            free_space = disk_usage(target).free
-            ic(free_space)
-            if disk_usage(target).free >= min_free_space:
-                yield target
-            else:
-                eprint("skipped:", target, "<", min_free_space)
-    raise FileNotFoundError
-
-
-#
-#
-#def count_entries(folder):  # fast, returns all types of objects in a folder
-#    return len(os.listdir(folder))
-#
-#
-
-
-#def count_files(folder):  # calls lstat on every entry to see if its a file
-#    total = 0
-#    for root, dirs, files in os.walk(folder):
-#        total += len(files)
-#    return total
-#
-#
-#def list_files(folder):
-#    all_files = []
-#    for root, dirs, files in os.walk(folder):
-#        for ifile in files:
-#            relative_file_path = root + b'/' + ifile
-#            all_files.append(relative_file_path)
-#    return set(all_files)
-
-
-#def path_is_dir(path):
-#    if os.path.isdir(path): #could still be a symlink
-#        if os.path.islink(path):
-#            return False
-#        return True
-#
-#
-#def path_is_dir_or_symlink_to_dir(path):
-#    # unlike os.path.exists(False), os.path.isdir(False) returns False
-#    if os.path.isdir(path): # returns False if it's a symlink to a file
-#        return True
-#    return False
-
-
