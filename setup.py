85a7f090374b8f3befde26e7c6064d3575c71634
jakeogh <github.com@v6y.net>
2022-03-03 00:57:18 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
 import fastentrypoints
-from setuptools import find_packages, setup
+
 if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+    sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
 dependencies = ["icecream", "click", "colorama", "click-command-tree"]
 
@@ -17,17 +21,12 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "Short explination of what it does _here_",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"pathtool": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"pathtool": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
     "install_requires": dependencies,
-    "entry_points": {
-        "console_scripts": [
-            "pathtool=pathtool.pathtool:cli",
-        ],
-    },
 }
 
 setup(**config)
