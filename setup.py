e7b8c6890205ea9f42b87a1c0fec218ca223a153
jakeogh <github.com@v6y.net>
2022-04-22 19:15:09 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import fastentrypoints
 if not sys.version_info[0] == 3:
     sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
-dependencies = ["icecream", "click", "colorama", "click-command-tree"]
+dependencies = ["icecream", "click", "colorama"]
 
 config = {
     "version": "0.1",
