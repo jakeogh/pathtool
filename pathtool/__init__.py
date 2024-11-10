b437da028676f2af77b0887deb1bed0bac68addc
jakeogh <github.com@v6y.net>
2024-11-10 02:04:40 -0700

    auto-commit

--- pathtool/__init__.py
+++ pathtool/__init__.py
@@ -3,37 +3,41 @@ isort:skip_file
 """
 
 # from .pathtool import is_unbroken_symlink_to_target
-from .pathtool import backup_file_if_exists
-from .pathtool import check_or_create_dir
-from .pathtool import comment_out_line_in_file
-from .pathtool import create_relative_symlink
-from .pathtool import delete_file_and_recreate_empty_immutable
-from .pathtool import empty_file
-from .pathtool import file_exists_nonzero
-from .pathtool import get_file_size
-from .pathtool import get_path_with_most_free_space
-from .pathtool import get_symlink_target_final
-from .pathtool import gurantee_symlink
-from .pathtool import is_broken_symlink
-from .pathtool import is_regular_file
-from .pathtool import is_unbroken_symlink
-from .pathtool import largest_file
-from .pathtool import make_file_immutable
-from .pathtool import make_file_not_immutable
-from .pathtool import mkdir_or_exit
-from .pathtool import path_exists
-from .pathtool import path_is_block_special
-from .pathtool import path_is_dir
-from .pathtool import path_is_file
-from .pathtool import paths_are_identical
-from .pathtool import points_to_data
-from .pathtool import read_file_bytes
-from .pathtool import really_is_dir
-from .pathtool import remove_empty_folders
-from .pathtool import symlink_destination
-from .pathtool import symlink_or_exit
-from .pathtool import target_generator
-from .pathtool import uncomment_line_in_file
-from .pathtool import wait_for_block_special_device_to_exist
-from .pathtool import wait_for_path_to_exist
-from .pathtool import write_line_to_file
+from .pathtool import backup_file_if_exists as backup_file_if_exists
+from .pathtool import check_or_create_dir as check_or_create_dir
+from .pathtool import comment_out_line_in_file as comment_out_line_in_file
+from .pathtool import create_relative_symlink as create_relative_symlink
+from .pathtool import (
+    delete_file_and_recreate_empty_immutable as delete_file_and_recreate_empty_immutable,
+)
+from .pathtool import empty_file as empty_file
+from .pathtool import file_exists_nonzero as file_exists_nonzero
+from .pathtool import get_file_size as get_file_size
+from .pathtool import get_path_with_most_free_space as get_path_with_most_free_space
+from .pathtool import get_symlink_target_final as get_symlink_target_final
+from .pathtool import gurantee_symlink as gurantee_symlink
+from .pathtool import is_broken_symlink as is_broken_symlink
+from .pathtool import is_regular_file as is_regular_file
+from .pathtool import is_unbroken_symlink as is_unbroken_symlink
+from .pathtool import largest_file as largest_file
+from .pathtool import make_file_immutable as make_file_immutable
+from .pathtool import make_file_not_immutable as make_file_not_immutable
+from .pathtool import mkdir_or_exit as mkdir_or_exit
+from .pathtool import path_exists as path_exists
+from .pathtool import path_is_block_special as path_is_block_special
+from .pathtool import path_is_dir as path_is_dir
+from .pathtool import path_is_file as path_is_file
+from .pathtool import paths_are_identical as paths_are_identical
+from .pathtool import points_to_data as points_to_data
+from .pathtool import read_file_bytes as read_file_bytes
+from .pathtool import really_is_dir as really_is_dir
+from .pathtool import remove_empty_folders as remove_empty_folders
+from .pathtool import symlink_destination as symlink_destination
+from .pathtool import symlink_or_exit as symlink_or_exit
+from .pathtool import target_generator as target_generator
+from .pathtool import uncomment_line_in_file as uncomment_line_in_file
+from .pathtool import (
+    wait_for_block_special_device_to_exist as wait_for_block_special_device_to_exist,
+)
+from .pathtool import wait_for_path_to_exist as wait_for_path_to_exist
+from .pathtool import write_line_to_file as write_line_to_file
