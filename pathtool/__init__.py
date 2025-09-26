dd0ecc551e6b8c7968780ae40ed3f801072656c7
jakeogh <github.com@v6y.net>
2025-09-25 19:22:58 -0700

    auto-commit

--- pathtool/__init__.py
+++ pathtool/__init__.py
@@ -5,12 +5,18 @@ isort:skip_file
 from .pathtool import ForceRequiredError as ForceRequiredError
 from .pathtool import SelfSymlinkError as SelfSymlinkError
 from .pathtool import UnableToSetImmutableError as UnableToSetImmutableError
-from .pathtool import calculate_relative_symlink_target as calculate_relative_symlink_target
+from .pathtool import (
+    calculate_relative_symlink_target as calculate_relative_symlink_target,
+)
 from .pathtool import chdir_or_exit as chdir_or_exit
-from .pathtool import create_file_with_exclusive_access as create_file_with_exclusive_access
+from .pathtool import (
+    create_file_with_exclusive_access as create_file_with_exclusive_access,
+)
 from .pathtool import create_relative_symlink as create_relative_symlink
 from .pathtool import create_symlink as create_symlink
-from .pathtool import delete_file_and_recreate_empty_immutable as delete_file_and_recreate_empty_immutable
+from .pathtool import (
+    delete_file_and_recreate_empty_immutable as delete_file_and_recreate_empty_immutable,
+)
 from .pathtool import format_bytes as format_bytes
 from .pathtool import get_free_space as get_free_space
 from .pathtool import get_path_with_most_free_space as get_path_with_most_free_space
@@ -28,7 +34,7 @@ from .pathtool import path_is_dir_or_symlink_to_dir as path_is_dir_or_symlink_to
 from .pathtool import path_is_file as path_is_file
 from .pathtool import path_is_file_or_symlink_to_file as path_is_file_or_symlink_to_file
 from .pathtool import path_is_symlink as path_is_symlink
+from .pathtool import path_is_block_special as path_is_block_special
 from .pathtool import resolve_symlink_final as resolve_symlink_final
 from .pathtool import temp_fifo as temp_fifo
 from .pathtool import walk_directory as walk_directory
-
